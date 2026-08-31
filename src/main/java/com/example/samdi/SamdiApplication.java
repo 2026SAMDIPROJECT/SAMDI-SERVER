4cdb273da64d824cc7d40b37c243e94789da72e5
홍정민 <s01046927968@gmail.com>
2026-08-31 19:25:17 +0900

    first commit

---
+++ src/main/java/com/example/samdi/SamdiApplication.java
@@ -0,0 +1,13 @@
+package com.example.samdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamdiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamdiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/samdi/SamdiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamdiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
